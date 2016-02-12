d09d834246c2115451ea09ed42a08611eaedacac
HybridDog <ovvv@web.de>
2016-02-12 17:22:48 +0100

    :cat2:

--- init.lua
+++ init.lua
@@ -17,6 +17,13 @@ for node,img in pairs(glass_list) do
 	end
 end
 
+--[[
+local bookshelf = minetest.registered_nodes["default:bookshelf"]
+if bookshelf then
+	bookshelf.tiles[2] = "connected_textures_bookshelf_middle.png"
+	minetest.override_item(node, {tiles = def.tiles, drawtype = "glasslike_framed_optional", paramtype = "light"})
+end--]]
+
 local time = math.floor(tonumber(os.clock()-load_time_start)*100+0.5)/100
 local msg = "[connected_textures] loaded after ca. "..time
 if time > 0.05 then
